dba7ebbcd3a8483d4f68d540db14e24bdea18442
Arturo Puente <arturo@able.co>
2019-06-01 22:01:57 -0500

    Bare minimum emacs and fish config

--- setup.sh
+++ setup.sh
@@ -36,3 +36,18 @@ brew tap heroku/brew
 brew tap d12frosted/emacs-plus
 brew install ${brew_dependencies[@]}
 brew cask install ${cask_apps[@]}
+
+mkdir -p ~/dev/projects/arturo
+git clone https://github.com/arturopuente/dotfiles.git ~/dev
+
+# Emacs
+git clone https://github.com/arturopuente/yasnippet-plus-ultra.git ~/dev/projects/arturo
+
+mkdir ~/.emacs.d
+touch ~/.emacs.d/custom.el
+ln -s ~/dev/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
+ln -s ~/dev/projects/arturo/yasnippet-plus-ultra ~/.emacs.d/snippets
+
+# Fish
+ln -s ~/dev/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -s ~/dev/dotfiles/fish/fish_variables ~/.config/fish/fish_variables
